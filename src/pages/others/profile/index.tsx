73b17767b604aa596959a5f5f1bdabffffe45512
jyotiReact <jyoti.itwaves@gmail.com>
2025-05-08 19:27:40 +0530

    worked on pending work on dashboard and profile

--- src/pages/others/profile/index.tsx
+++ src/pages/others/profile/index.tsx
@@ -29,7 +29,7 @@ const Profile = () => {
 
   return (
     <>
-      <div className="overflow-hidden border border-gray-100 bg-white shadow-2xl">
+      <div className="overflow-hidden border border-gray-100 bg-white shadow-2xl h-screen">
         <div className="relative z-20 h-40 md:h-60">
           <div className="h-full w-full  bg-gradient-to-r from-pink-300 via-purple-300 to-pink-300 backdrop-blur-sm"></div>
         </div>
